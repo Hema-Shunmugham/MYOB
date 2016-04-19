5387b1173ee492d0d7db64b7d5b61cf8fd657e86
Hema-Shunmugham <hemalatha.pavandeep@gmail.com>
2016-04-20 00:14:14 +1000

    Added files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# MYOB
+### Run the Application
+
+To start the app:
+```
+npm start
+```
+
+Now browse to the app at `http://localhost:8000/app/index.html`.
+
+### Running Unit Tests
+```
+npm test
+```
+
+
+
+
+
+
+
---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/ng-lodash/build/ng-lodash.js',
+      'app/app.js',
+      'app/inputView/**/*.js',
+      'app/payslipView/**/*.js',
+      'app/payslipView/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+  });
+};
